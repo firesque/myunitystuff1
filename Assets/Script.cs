4b08326fb8574f96231c59730dea12e53bbe919f
firesque <33391949+firesque@users.noreply.github.com>
2024-01-01 21:00:36 -0500

    Add files via upload

---
+++ Assets/Script.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    string Name = "Evan";
+    // Start is called before the first frame update
+    int life = 5;
+    void Awake()
+    {
+        Debug.Log("Hello");
+    }
+    void Start()
+    {
+        Debug.Log("World");
+        Shoot(5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Name);
+        if (life < 1) {
+            Debug.Log("Game Over");
+                }
+        else {
+            Debug.Log("Playing");
+        }
+    }
+
+    void Shoot(int data)
+    {
+        Debug.Log("Shooting");
+        Debug.Log(data);
+    }
+
+}
